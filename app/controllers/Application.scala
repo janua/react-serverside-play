d363dd7a7b9789802ccf55d621d0b5e19713ca50
Francis Carr <fran@fcarr.com>
2015-06-20 17:32:49 +0100

    Change backendState to an object Add back in global = this

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,17 +14,11 @@ object Application extends Controller {
     maybeEngine match {
       case None => BadRequest("Nashorn script engine not found. Are you using JDK 8?")
       case Some(engine) =>
-        // React expects `window` or `global` to exist. Create a `global` pointing
-        // to Nashorn's context to give React a place to define its global namespace.
-        engine.eval("var backendState = this;")
+        engine.eval("var global = this;")
+        engine.eval("var backendState = {};")
         engine.eval("backendState.title = 'List of posts';")
         engine.eval(s"backendState.posts = ${Json.stringify(Json.toJson(Blog.allPosts))};")
 
-        //val posts = new SimpleBindings()
-        //posts.put("posts", "[{title: 'From engine put'}]")
-        //engine.setBindings(posts, ScriptContext.GLOBAL_SCOPE)
-
-        // Evaulate React and the application code.
         engine.eval(new FileReader("target/web/web-modules/main/webjars/lib/react/react-with-addons.js"))
         engine.eval(new FileReader("target/web/web-modules/main/webjars/lib/lodash/lodash.min.js"))
         engine.eval(new FileReader("target/web/public/main/javascripts/Blog.js"))
